44d630cfc0dffc4fe1304e2a00d880aba7070908
xiebro144 <137803609+xiebro144@users.noreply.github.com>
2024-03-10 14:09:40 +0800

    Feat: return promise，update tests (#1)
    * Feat: return promise
    
    * Test: update test cases

--- index.test.js
+++ index.test.js
@@ -3,79 +3,57 @@ const createEVMWalletCsv = require('./index');
 const fs = require('fs');
 const csv = require('csv-parser');
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 describe('createEVMWalletCsv', () => {
-  // test('should create the correct number of wallets', () => {
-  //   const wallets = createEVMWalletCsv(5);
-  //   expect(wallets.length).toBe(5);
-  // });
-
-  // test('each wallet should have a index, a private key and an address', () => {
-  //   const wallets = createEVMWalletCsv();
-  //   expect(wallets.length).toBe(100);
-  //   wallets.forEach((wallet, i) => {
-  //     expect(wallet).toHaveProperty('privateKey');
-  //     expect(wallet).toHaveProperty('address');
-  //     expect(wallet).toHaveProperty('index', i);
-  //   });
-  // });
-
-  // test('each wallet should have an index if index parameter is false', () => {
-  //   const wallets = createEVMWalletCsv({ index: false });
-  //   wallets.forEach((wallet, i) => {
-  //     expect(wallet).not.toHaveProperty('index');
-  //   });
-  // });
-
-  // test('each wallet should have a pk prop and an addr prop ', () => {
-  //   const wallets = createEVMWalletCsv({
-  //     privateKeyProps: 'pk',
-  //     addressProps: 'addr'
-  //   });
-  //   expect(wallets.length).toBe(100);
-  //   wallets.forEach((wallet, i) => {
-  //     expect(wallet).toHaveProperty('pk');
-  //     expect(wallet).toHaveProperty('addr');
-  //   });
-  // });
-
-
-  test('should read and parse the CSV file correctly', async done => {
+  test('should read and parse the CSV file correctly', async () => {
     const stamp = Date.now();
     const csvpath = `./w1-${stamp}.csv`
-    console.log(csvpath)
-    createEVMWalletCsv({
+    await createEVMWalletCsv({
       number: 10,
       path: csvpath,
     });
-    // Wait for 2 seconds before reading the file
-    // await sleep(200);
 
     const results = [];
-    const fs = require('fs');
 
-    fs.watchFile(csvpath, (curr, prev) => {
-      console.log('File changed');
-      // You can add your code here to handle the file change
-
-      fs.createReadStream(csvpath)
-        .pipe(csv())
-        .on('data', (data) => console.log(data.index), results.push(data))
-        .on('end', () => {
-          console.log(results)
-          expect(results.length).toBeGreaterThan(0);
-          expect(results.length).toBe(10);
-          results.forEach((row, i) => {
-            expect(row).toHaveProperty('index');
-            expect(wallet).toHaveProperty('privateKey');
-            expect(wallet).toHaveProperty('address');
-          });
-          done();
+    fs.createReadStream(csvpath)
+      .pipe(csv())
+      .on('data', (data) => {
+        results.push(data);
+      })
+      .on('end', () => {
+        expect(results.length).toBeGreaterThan(0);
+        expect(results.length).toBe(10);
+        results.forEach((row, i) => {
+          expect(row).toHaveProperty('index', i.toString());
+          expect(row).toHaveProperty('privateKey');
+          expect(row).toHaveProperty('address');
         });
+      });
+
+  });
+
+  test('should read and parse the CSV file with custom parameter correctly', async () => {
+    const stamp = Date.now();
+    const csvpath = `./w2-${stamp}.csv`
+    await createEVMWalletCsv({
+      path: csvpath,
+      privateKeyProps: 'pk',
+      addressProps: 'addr'
     });
 
-  }, 10000);
+    const results = [];
+
+    fs.createReadStream(csvpath)
+      .pipe(csv())
+      .on('data', (data) => {
+        results.push(data);
+      })
+      .on('end', () => {
+        expect(results.length).toBe(100);
+        results.forEach((row) => {
+          expect(row).toHaveProperty('pk');
+          expect(row).toHaveProperty('addr');
+        });
+      });
+
+  });
 });
